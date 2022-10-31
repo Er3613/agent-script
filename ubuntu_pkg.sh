58413004f08e4c1239255ddc0cd9bb7d38d1279c
Er3613 <penneric220@gmail.com>
2022-10-31 09:35:32 +0200

    ubuntu pkg installation

---
+++ ubuntu_pkg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+    #Author : Utrains
+    #Date : 01-Nov-2021
+
+## ---------- script that Install some packages in Linux -----------------
+
+apt install finger -y
+apt install curl -y
+apt install zip -y
+apt install vim -y
